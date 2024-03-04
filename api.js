9f85c2c167715cc1b288ef0bbd8f281062eff888
rebahaider <rebahaiderema@gmail.com>
2024-03-05 04:48:19 +0600

    Join our forum section

--- api.js
+++ api.js
@@ -7,10 +7,10 @@ const allPosts = async () => {
 allPosts();
 
 const displayPost = posts => {
-    console.log(posts);
+    //console.log(posts);
     const discussTopic = document.getElementById('discuss-topics');
     posts.forEach(post => {
-        console.log(post);
+        //console.log(post);
         const div = document.createElement('div');
         div.innerHTML = `
         <div class=" lg:flex rounded-xl bg-gray-200 shadow-xl">
@@ -40,4 +40,49 @@ const displayPost = posts => {
         `
         discussTopic.appendChild(div);
     });
+}
+
+const latestPost = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await response.json();
+    displayLatestPost(data);
+}
+latestPost();
+
+const displayLatestPost = data => {
+    console.log(data);
+    const latestPostContainer = document.getElementById('latest-post-container');
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        console.log(element);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+              <img src="${element.cover_image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">${element?.author?.posted_date || 'No publish date'}</h2>
+              <p class="font-bold text-xl">${element.title}</p>
+              <p>${element.description}</p>
+              <div class="flex">
+              <div>
+              <figure class="px-10 pt-10">
+              <img src="${element.profile_image}" class="w-[80px] h-[80] rounded-full" alt="Picture" class="rounded-xl" />
+            </figure>
+            </div>
+            <div class="mt-12">
+            <p class="font-bold text-xl">${element?.author?.name}</p>
+            <p>${element?.author?.designation || 'Unknown'}</p>
+            </div>
+            
+              </div>
+            </div>
+          </div>
+    
+        `
+        latestPostContainer.appendChild(div);
+        
+    }
+    
 }
